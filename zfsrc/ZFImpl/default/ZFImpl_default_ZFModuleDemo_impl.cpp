c9c7dae6e6594320b22aa039fa9c99415c96d7c4
ZSaberLv0 <z@zsaber.com>
2018-05-17 17:00:08 +0800

    first commit

---
+++ zfsrc/ZFImpl/default/ZFImpl_default_ZFModuleDemo_impl.cpp
@@ -0,0 +1,22 @@
+/* ====================================================================== *
+ * Copyright (c) 2010-2018 ZFFramework
+ * Github repo: https://github.com/ZFFramework/ZFFramework
+ * Home page: http://ZFFramework.com
+ * Blog: http://zsaber.com
+ * Contact: master@zsaber.com (Chinese and English only)
+ * Distributed under MIT license:
+ *   https://github.com/ZFFramework/ZFFramework/blob/master/LICENSE
+ * ====================================================================== */
+#include "ZFImpl_default_ZFModuleDemo_impl.h"
+#include "ZFCore.h"
+
+ZF_NAMESPACE_GLOBAL_BEGIN
+
+ZF_GLOBAL_INITIALIZER_INIT(ZFModuleDemo_impl_test)
+{
+    zfLogT() << zfText("I'm contents from ZFModuleDemo_impl");
+}
+ZF_GLOBAL_INITIALIZER_END(ZFModuleDemo_impl_test)
+
+ZF_NAMESPACE_GLOBAL_END
+
