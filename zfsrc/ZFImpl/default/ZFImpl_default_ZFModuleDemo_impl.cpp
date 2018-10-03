3e34e8dd2af502c85ec87930f814113da250dc22
ZSaberLv0 <z@zsaber.com>
2018-10-03 14:17:29 +0100

    update

--- zfsrc/ZFImpl/default/ZFImpl_default_ZFModuleDemo_impl.cpp
+++ zfsrc/ZFImpl/default/ZFImpl_default_ZFModuleDemo_impl.cpp
@@ -15,15 +15,15 @@ ZF_NAMESPACE_GLOBAL_BEGIN
 
 zfstring demoFuncFromImpl(void)
 {
-    zfstring ret = zfText("I'm string from ZFModuleDemo_impl");
-    ret += zfText(", and I'm string from ZFModuleDemo_lib: ");
+    zfstring ret = "I'm string from ZFModuleDemo_impl";
+    ret += ", and I'm string from ZFModuleDemo_lib: ";
     ret += demoFunc();
     return ret;
 }
 
 ZF_GLOBAL_INITIALIZER_INIT(ZFModuleDemo_impl_test)
 {
-    zfLogT() << zfText("I'm contents from ZFModuleDemo_impl");
+    zfLogT() << "I'm contents from ZFModuleDemo_impl";
 }
 ZF_GLOBAL_INITIALIZER_END(ZFModuleDemo_impl_test)
 
