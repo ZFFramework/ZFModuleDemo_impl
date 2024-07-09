86ac8906165bfb169a4a73a2005b253b13a5d56f
ZSaberLv0 <z@zsaber.com>
2024-07-09 22:43:24 +0800

    update

---
+++ zfsrc/ZFImpl/default/ZFProtocolZFModuleDemo_default.cpp
@@ -0,0 +1,14 @@
+#include "ZFImpl_default_ZFModuleDemo_impl.h"
+#include "ZFModuleDemo_lib/protocol/ZFProtocolZFModuleDemo.h"
+
+ZF_NAMESPACE_GLOBAL_BEGIN
+
+ZFPROTOCOL_IMPLEMENTATION_BEGIN(ZFModuleDemoImpl_default, ZFModuleDemo, ZFProtocolLevel::e_Default)
+public:
+    virtual zfstring demoImpl(void) {
+        return "string from ZFModuleDemoImpl";
+    }
+ZFPROTOCOL_IMPLEMENTATION_END(ZFModuleDemoImpl_default)
+
+ZF_NAMESPACE_GLOBAL_END
+
